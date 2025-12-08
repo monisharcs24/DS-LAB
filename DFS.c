80dc5619f881a8af6a49bc2d135e5808f0e08895
monisharcs24 <monishar.cs24@bmsce.ac.in>
2025-12-08 11:54:37 +0530

    Lab 9 DFS

---
+++ DFS.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#define MAX 100
+
+int visited[MAX];
+void DFS(int adj[MAX][MAX], int n, int v) {
+    visited[v] = 1;
+
+    for (int i = 0; i < n; i++) {
+        if (adj[v][i] == 1 && !visited[i]) {
+            DFS(adj, n, i);
+        }
+    }
+}
+int main() {
+    int n, adj[MAX][MAX];
+
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &adj[i][j]);
+        }
+    }
+    for (int i = 0; i < n; i++) {
+        visited[i] = 0;
+    }
+    DFS(adj, n, 0);
+
+    int connected = 1;
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            connected = 0;
+            break;
+        }
+    }
+    if (connected)
+        printf("The graph is CONNECTED.\n");
+    else
+        printf("The graph is NOT CONNECTED.\n");
+    return 0;
+}
